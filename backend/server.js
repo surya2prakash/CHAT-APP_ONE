a97e02a22764c3824bff015d9f5db4d778558100
surya2prakash <sv251171@gmail.com>
2025-08-16 17:01:55 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,54 @@
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const {Server} = require("socket.io");
+const http = require("http");
+
+
+
+
+const server = http.createServer(app) ;
+
+// app.use(cors({
+//     origin:"http://localhost:3000",
+//     credentials:true,
+//     methods:["GET","POST"]
+// }))
+
+
+const io = new Server(server,cors({
+    origin:"*",
+    credentials:true,
+    methods:["GET","POST"]
+}));
+
+io.on('connection',(socket)=>{
+    console.log("User Connected :",socket.id);
+    socket.on('message',(data)=>{
+        console.log("Message :",data);
+        socket.broadcast.emit("message",data);
+    });
+
+    socket.on("typing",()=>{
+
+        socket.broadcast.emit("typing",{user:socket.id})
+    })
+     
+    socket.on("stopTyping",()=>{
+        socket.broadcast.emit("stopTyping",{user:socket.id});
+    })
+    
+socket.on("disconnect",()=>{
+    console.log("User disconnect ho gya :",socket.id);
+})
+})
+
+
+server.listen(5000,()=>{
+    console.log("Server is connected..")
+})
+
+
+
+
